b8b8b3b66a40b4575f57a27a00850e95209a4f3f
Ahmet <ahmetboyraz.de@gmail.com>
2023-01-31 19:05:02 +0100

    thumbnail downloader

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  if(isset($_POST['button'])){
+    $imgUrl = $_POST['imgurl'];
+    $ch = curl_init($imgUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $downloadImg = curl_exec($ch);
+    curl_close($ch);
+    header('Content-type: image/jpg');
+    header('Content-Disposition: attachment;filename="thumbnail.jpg"');
+    echo $downloadImg;
+  }
+?>
+<!DOCTYPE html>
+<!-- Coding By AhmetBoyraz - https://github.com/AhmetBoyrazz -->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Download YouTube Video Thumbnail | CodingNepal</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
